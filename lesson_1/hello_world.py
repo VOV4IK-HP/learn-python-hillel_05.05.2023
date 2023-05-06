4974b0c7ad3af2fa57c6bd030462992c04c6b288
VOV4IK-HP <shaliginvn@gmail.com>
2023-05-06 21:10:56 +0300

    заливаем первые файлы, проверка

---
+++ lesson_1/hello_world.py
@@ -0,0 +1,18 @@
+# Литералы - это любые знаечения, не записанные в переменные. Наример: 5, -1, 1000, 5.3, "Hello World!"
+
+print(5)
+
+print('Hello World!')
+print("Hello World!")
+
+# Переменные - это место в памяти, названное так, как этого хочет програмист
+x = 5
+print(x)
+x = 3
+print(x)
+
+name=input('Wtat is your name?')
+print('Hello? name')
+print('Hello, ', name)
+print(f'Hello, {name}')
+print(f"Hello, {name}")
