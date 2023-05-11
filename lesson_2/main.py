67fe2c198f85d62afc4fc14a58b5f21d6727a21f
VOV4IK-HP <shaliginvn@gmail.com>
2023-05-11 15:39:47 +0300

    учим переменные

--- lesson_2/main.py
+++ lesson_2/main.py
@@ -1,3 +1,62 @@
-a = 5
+a = 5     # int (integer) тип данных цельночисловые
 b = a + 3
 print(b)
+
+print(id(b))
+
+print(type(a))
+
+print(a)
+print(b)
+print('Сумма: ')
+print(a + b)
+print('Вычитание: ')
+print(a - b)
+print('Умножение: ')
+print(a * b)
+print('Деление: ')
+print(a / b)
+print('Деление целочисленное: ')
+print(a // b)
+print('Возведение в степень: ')
+print(a ** b)
+
+a = 5
+b = 8
+c = b == a + 3  # c = bool
+print(c)
+first = True
+second = False
+print(first == second)
+
+num = 1.5e2
+print(num)
+
+my_string = "2"
+print(my_string)
+
+my_string = "2"
+my_integer = int(my_string)
+print(my_integer + 3)
+
+my_float = 3.7
+my_integer = int(my_float)
+print(my_integer)
+
+num = 13 + 1j
+
+print(type(num))
+print(num.real)
+print(num.imag)
+
+_true = True
+_false = False
+print(_true == _false)
+
+first = 4
+second = 2
+print(first == second)
+print(4 > 2)
+print(4 > 2)
+print(4 >= 2)
+print(4 <= 2)
