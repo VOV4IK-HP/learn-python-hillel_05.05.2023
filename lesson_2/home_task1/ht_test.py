8f5820c1ee25f5f9bc2fd4fabe422896d34132fc
VOV4IK-HP <shaliginvn@gmail.com>
2023-05-12 12:15:02 +0300

    ДЗ 1 тест + задачи

---
+++ lesson_2/home_task1/ht_test.py
@@ -0,0 +1,25 @@
+x = 7
+print(x - 2.0)
+
+x = 5
+print(x ** 2)
+
+x = 36
+print(pow(x, 1/2))
+
+x = 10
+print('x // 2')
+
+x = divmod(11, 2)
+print(x)
+
+x = 5
+print(type(x))
+
+x = 5 / 2
+print(type(x))
+
+import math
+print(round(math.pi, 2))  # число Пи - 3.141592653589793
+
+print(int(3.9))
