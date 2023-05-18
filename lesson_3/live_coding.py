c4e557207bb14aecb8503fb24ce41b6943b49788
VOV4IK-HP <shaliginvn@gmail.com>
2023-05-18 15:34:53 +0300

    Lesson 3

--- lesson_3/live_coding.py
+++ lesson_3/live_coding.py
@@ -1,9 +1,9 @@
-input_message = input("введи строку : ")
-print(input_message[::-1])
+#input_message = input("введи строку : ")
+#print(input_message[::-1])
 
-input_name = input("Введи имя: ")
-answer = f"Hello, {input_name}"
-print(answer)
+#input_name = input("Введи имя: ")
+#answer = f"Hello, {input_name}"
+#print(answer)
 
 word = input("Write your word: ")
 word = word.lower()
