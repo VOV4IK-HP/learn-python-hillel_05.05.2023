948719573fd8f9c28ba106b99ea64189aea7252d
VOV4IK-HP <shaliginvn@gmail.com>
2023-05-20 18:03:31 +0300

    ДЗ #2

--- home_task2/Task_1.py
+++ home_task2/Task_1.py
@@ -12,7 +12,6 @@ str = str.replace(")", "")
 str_what = input('What do you want to replace?\n-> ')
 word = str.find(str_what)
 print(str_what, 'was found at position', word)
-
 str_with = input('With what do you want to replace?\n-> ')
 str = str.replace(str_what, str_with)
 print('Here is your result:\n->', str)
