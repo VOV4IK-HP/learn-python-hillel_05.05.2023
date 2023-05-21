68d36d8ec8da904cca94707509bcdc2171009290
VOV4IK-HP <shaliginvn@gmail.com>
2023-05-21 19:18:17 +0300

    ДЗ #3 БОТ

--- home_task3/Bot_communication.py
+++ home_task3/Bot_communication.py
@@ -3,13 +3,13 @@ print("Приємно познайомитися,", name,"!")
 while True:
     request = input(name+': ')
     request = request.lower()
-    if request == 'хай'or request == 'доброго' or request == 'вітаю' or request == 'привіт':
+    if 'хай' in request or 'привіт' in request or 'доброго' in request or 'вітаю' in request:
         print('БОТ: Доброго вечора, я бот з України!')
-    if request == 'як справи?' or request == 'що робиш?' or request == 'чим займаєшся?':
+    if 'як' in request or 'що' in request or 'чим' in request:
         print('БОТ: Вчусь програмувати на Python!')
-    if request == 'фільм' or request == 'серіал' or request == 'кіно':
-        print('БОТ: Соррі що втручаюсь, не знаю про що йдеться мова, але подивись фільм ""Банди Нью-Йорка"", він просто бомба!')
-    if request == 'бот рекомендує фільм' or request == 'бот рекомендує серіал':
-        print('БОТ: Соррі що втручаюсь, не знаю про що йдеться мова, але подивись фільм ""Банди Нью-Йорка"", він просто бомба!')
-    if request == 'бувай' or request == "добре" or request == "до зустрічі":
+    if 'фільм' in request or 'серіал' in request or 'кіно' in request:
+        print('БОТ: Подивись фільм ""Банди Нью-Йорка"", він просто бомба!')
+    if 'бот рекомендує' in request or 'круто' in request or 'ого' in request:
+        print('БОТ: Соррі що втручаюсь, не знаю про що йдеться мова,\n але подивись фільм ""Банди Нью-Йорка"", він просто бомба!')
+    if 'бувай' in request or "добре" in request or "до зустрічі" in request:
         exit('БОТ: Побачимось у мережі, Coming soon.')
