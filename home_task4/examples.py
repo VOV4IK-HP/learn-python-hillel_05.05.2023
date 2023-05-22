221ec2bf443459ec2e3bafe8f14bec0a9699066b
VOV4IK-HP <shaliginvn@gmail.com>
2023-05-22 20:43:45 +0300

    independent work

---
+++ home_task4/examples.py
@@ -0,0 +1,31 @@
+# task 1
+# name = input('What is your name?\n')
+# print ("Hallo", name)
+
+# task 1
+#name = input('What is your name?\n-> ')
+#surname = input('What is your surname?\n-> ')
+#print ("Hallo,", name, surname)
+
+# task 3
+#dialog = input('What do you call a bear with no teeth?\nA gummy bear!')
+#print(dialog)
+
+# task 4
+#num1 = int(input('Please enter your first number \n-> '))
+#num2 = int(input('Please enter your second number \n-> '))
+#answer = num1 + num2
+#print('The total is', answer)
+
+# task 5
+#num1 = int(input('Please enter your first number \n-> '))
+#num2 = int(input('Please enter your second number \n-> '))
+#num3 = int(input('Please enter your third number \n-> '))
+#answer = (num1 + num2) * num3
+#print('The total is', answer)
+
+# task 6
+num1 = int(input('How many slices of pizza did you have? \n-> '))
+num2 = int(input('How many slices of pizza did you eat? \n-> '))
+num3 = num1 - num2
+print ("You have", num3, 'slices of pizza left!')
