b182e9e582f10a22a003380708ca79e1de4d9c42
VOV4IK-HP <shaliginvn@gmail.com>
2023-06-26 22:07:10 +0300

    ДЗ #9

---
+++ home_task9/draft.py
@@ -0,0 +1,19 @@
+# имя файла
+FILENAME = "notes.txt"
+# определяем пустой список
+notes = list()
+
+for i in range(3):
+    note = input("Add a notes " + str(i + 1) + ": ")
+    notes.append(note + "\n")
+
+# запись списка в файл
+with open(FILENAME, "a") as file:
+    for note in notes:
+        file.write(note)
+
+# считываем сообщения из файла
+print("Read notes")
+with open(FILENAME, "r") as file:
+    for note in file:
+        print(note, end="")
