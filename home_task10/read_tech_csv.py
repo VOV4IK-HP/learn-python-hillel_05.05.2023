5c57434a8c43c032489a9ff2e28c255ae9d1ea28
VOV4IK-HP <shaliginvn@gmail.com>
2023-07-15 16:06:43 +0300

    ДЗ #10

--- home_task10/read_tech_csv.py
+++ home_task10/read_tech_csv.py
@@ -1,6 +1,8 @@
 import csv
 import random
 
+# программа читает файл tech_inventory.csv с товарами интернет магазина с параметрами товара:
+# model, category, brand, price
 
 def read_csv(file):
     data = []
@@ -10,7 +12,8 @@ def read_csv(file):
             data.append(row)
     return data
 
-
+# создаем индекс уникальных айди для каждой записи, то есть словарь,
+# где ключи – это сгенерированные уникальные айди, а значение – полная информация о позиции товара
 def generate_id():
     return random.randint(1, 1000)
 
@@ -23,7 +26,8 @@ def create_id_index(data):
         id_index[item_id] = item
     return id_index
 
-
+# создаем индекс по категориям. То есть словарь, где ключи – это название категории/бренда,
+# а значение – это перечень уникальных айди товаров, в которых есть такое значение поля категории/бренда
 def create_category_index(data):
     category_index = {}
     for item in data:
@@ -33,7 +37,7 @@ def create_category_index(data):
         category_index[category].append(item['id'])
     return category_index
 
-
+# # создаем индекс по категориям и брендам
 def create_brand_index(data):
     brand_index = {}
     for item in data:
@@ -43,17 +47,18 @@ def create_brand_index(data):
         brand_index[brand].append(item['id'])
     return brand_index
 
-
+# выводит на экран статистику сколько товаров от каждого бренда
 def print_brand_stats(brand_index):
     for brand, ids in brand_index.items():
         print(f"{brand}: {len(ids)} товаров")
 
-
+#
+# выводит на экран статистику сколько товаров от каждой категории
 def print_category_stats(category_index):
     for category, ids in category_index.items():
         print(f"{category}: {len(ids)} товаров")
 
-
+# cчитаем распределение товаров по брендам для каждой категории и выводит это на экран
 def print_items_by_brand_and_category(data, brand, category):
     items = [item for item in data if item['brand'] == brand and item['category'] == category]
     for item in items:
@@ -83,22 +88,23 @@ def print_brand_distribution_by_category(data):
         print()
 
 
-data = read_csv('tech_inventory.csv')
-id_index = create_id_index(data)
-category_index = create_category_index(data)
-brand_index = create_brand_index(data)
+if __name__ == "__main__":
+    data = read_csv('tech_inventory.csv')
+    id_index = create_id_index(data)
+    category_index = create_category_index(data)
+    brand_index = create_brand_index(data)
 
-print("Статистика по брендам:")
-print_brand_stats(brand_index)
-print()
+    print("Статистика по брендам:")
+    print_brand_stats(brand_index)
+    print()
 
-print("Статистика по категориям:")
-print_category_stats(category_index)
-print()
+    print("Статистика по категориям:")
+    print_category_stats(category_index)
+    print()
 
-print("Товары выбранного бренда и категории:")
-print_items_by_brand_and_category(data, 'Apple', 'Смартфоны')
-print()
+    print("Товары выбранного бренда и категории:")
+    print_items_by_brand_and_category(data, 'Apple', 'Смартфоны')
+    print()
 
-print("Распределение товаров по брендам для каждой категории:")
-print_brand_distribution_by_category(data)
+    print("Распределение товаров по брендам для каждой категории:")
+    print_brand_distribution_by_category(data)
